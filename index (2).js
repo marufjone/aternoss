987e523fecb36087d8372bd85dfedc125f46d444
marufjone <marufjonmamurov2o11@gmail.com>
2026-06-24 15:32:25 +0500

    Add files via upload

---
+++ index (2).js
@@ -0,0 +1,36 @@
+const mineflayer = require('mineflayer');
+const express = require('express'); // 24/7 ishlash uchun qo'shildi
+const app = express();
+
+// UptimeRobot ushbu manzilga bog'lanib botni uyg'oq tutadi
+app.get('/', (req, res) => {
+    res.send('Bot 24/7 rejimda faol!');
+});
+app.listen(3000, () => console.log('Veb-server 3000 portda tayyor.'));
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'marufjontechno.aternos.me', 
+        port: 25565, 
+        username: 'AFK_Bot_Uz', 
+        version: '1.21.1' 
+    });
+
+    bot.on('spawn', () => {
+        console.log('Bot muvaffaqiyatli kirdi va AFK rejimida!');
+        // Har 10 soniyada sakrab turish funksiyasi
+        setInterval(() => {
+            bot.setControlState('jump', true);
+            setTimeout(() => bot.setControlState('jump', false), 500);
+        }, 10000);
+    });
+
+    bot.on('end', () => {
+        console.log('Bot o\'chdi, 5 soniyadan keyin qayta ulanadi...');
+        setTimeout(createBot, 5000);
+    });
+
+    bot.on('error', (err) => console.log('Xatolik yuz berdi:', err));
+}
+
+createBot();
